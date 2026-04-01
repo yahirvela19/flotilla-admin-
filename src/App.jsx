78a98fbc23cecca4be769a2a6b8530ede876ba70
Yahir Vela <velayahir5@gmail.com>
2026-04-01 12:20:30 -0600

    Inicializacion del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-500">
+      <h1 className="text-4xl font-bold text-white shadow-lg p-4 rounded-xl bg-blue-700">
+        ¡FLOTILLA! 
+      </h1>
+    </div>
+  )
+}
