c517f6610718c180544b1221e7b2fdd8ca028cd5
yahirvela19 <velayahir5@gmail.com>
2026-04-04 12:03:51 -0600

    Delete flotilla-admin-system directory

--- flotilla-admin-system/src/components/ui/Card.jsx
+++
@@ -1,7 +0,0 @@
-export default function Card({ children }) {
-  return (
-    <div className="bg-cards  rounded-2xl p-6 shadow-xl shadow-black/10">
-      {children}
-    </div>
-  );
-}
--- flotilla-admin-system/src/pages/Pagos.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Pagos = () => {
-  return <div>Pantalla de Pagos</div>;
-};
-
-export default Pagos; 
